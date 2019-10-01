f2bc4e9c10a95ae2e243c50cc67aae382caaddee
a19vanesaab <a19vanesaab@fpteis.es>
2019-10-01 18:59:51 +0200

    añadido más hola mundo

--- HolaMundo/src/holamundo.java
+++ HolaMundo/src/holamundo.java
@@ -3,6 +3,9 @@ public class holamundo {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
+		System.out.println("Hola Mundo");
+		System.out.println("Hola Mundo");
+		System.out.println("Hola Mundo");
 		Coche coche = new Coche();
 		coche.saludar();
 	}
